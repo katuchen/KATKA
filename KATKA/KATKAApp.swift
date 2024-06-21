143053110dc25c9748421a5cbf43eeaeaa4cc9e3
katuchen <kate02kuz@icloud.com>
2024-06-21 16:20:26 +0300

    Initial Commit

---
+++ KATKA/KATKAApp.swift
@@ -0,0 +1,20 @@
+//
+//  KATKAApp.swift
+//  KATKA
+//
+//  Created by Екатерина Кузмичева on 21.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct KATKAApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
